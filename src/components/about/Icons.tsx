7f951ffbaab46a99180e11e5a076eca68bcce2d2
Jason Meredith <jasonmeredith93@gmail.com>
2024-11-23 16:49:02 +0000

    Revert "stop shimmer on mouseover"
    This reverts commit 056413b19a300284d41303046caa1c1f8eed6486.

--- src/components/about/Icons.tsx
+++ src/components/about/Icons.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
 
 function Canada() {
-  return <h1 className="text-xl font-bold">Canadian</h1>;
+  return (
+    <h1 className="text-xl font-bold">Canadian</h1>
+  )
 }
 
 function Camping() {
-  return <h1 className="text-xl font-bold">Camping</h1>;
+  return (
+    <h1 className="text-xl font-bold">Camping</h1>
+  )
 }
 
 function HoverableEmoji({
@@ -19,27 +22,15 @@ function HoverableEmoji({
   delay: string;
   setInfo: (info: React.ReactNode) => void;
 }) {
-  const [isHovered, setIsHovered] = React.useState(false);
-
   return (
     <span
       className="relative group cursor-pointer"
-      onMouseEnter={() => {
-        setIsHovered(true);
-        setInfo(children);
-      }}
-      onMouseLeave={() => {
-        setIsHovered(false);
-        setInfo(null);
-      }}
+      onMouseEnter={() => setInfo(children)}
+      onMouseLeave={() => setInfo(null)}
     >
       <span
-        className={`text-5xl transition duration-100 ease-in-out filter ${isHovered ? 'grayscale-0' : 'grayscale-[60%]'
-          }`}
-        style={{
-          animation: isHovered ? 'none' : `beckon 2s ease-in-out infinite`,
-          animationDelay: delay,
-        }}
+        className={`text-5xl transition duration-100 ease-in-out filter group-hover:grayscale-0 animate-beckon`}
+        style={{ animationDelay: delay }}
         role="img"
         aria-label="emoji"
       >
@@ -52,7 +43,7 @@ function HoverableEmoji({
 export default function Icons(props: { setInfo: (info: React.ReactNode) => void }) {
   let delay = 0;
 
-  const icons = [
+  const icons = ([
     { emoji: "🇨🇦", info: <Canada /> },
     { emoji: "🏕️", info: <Camping /> },
     { emoji: "☕️", info: <Camping /> },
@@ -67,13 +58,14 @@ export default function Icons(props: { setInfo: (info: React.ReactNode) => void 
     { emoji: "🎸", info: <Canada /> },
   ].map(({ emoji, info }) => {
     const delayValue = `${delay}s`;
-    delay += 0.2;
+    delay += 0.25;
     return (
       <HoverableEmoji setInfo={props.setInfo} key={emoji} emoji={emoji} delay={delayValue}>
         {info}
       </HoverableEmoji>
-    );
-  });
+    )
+  }
+  ))
 
-  return <div className="flex flex-wrap gap-2">{icons}</div>;
+  return icons
 }
